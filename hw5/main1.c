3e3777237255da1408ea435eb8d73c6a61735c3b
Boris-1008 <boris0905146617@gmail.com>
2024-12-12 13:01:45 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -3,12 +3,11 @@
 int main() {
     int n = 7;  // 金字塔的行數
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n + i - 1; j++) {
-            if (j <= n - i) {
-                printf(" ");
-            } else {
-                printf("%d", i);
-            }
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++){
+            printf("%d ", i);
         }
         printf("\n");
     }
